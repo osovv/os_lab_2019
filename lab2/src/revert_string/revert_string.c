8b42d2feb190d4f3f3379b13f546de7028dbf197
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-02-16 11:52:03 +0300

    lab 2 task 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,35 @@
 #include "revert_string.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 void RevertString(char *str)
 {
 	// your code here
+    int count, end, i;
+    count = 0;
+    while (str[count] != '\0')
+        count++;
+    end = count - 1;
+    char c;
+    for (i = 0; i < count / 2; i++)
+    {
+        c = str[i];
+        str[i] = str[end-i];
+        str[end-i] = c;
+    }
+    /*
+    char* tmp;
+    tmp = (char*)malloc((count+1) * sizeof(char));
+    for (i = 0; i < count; i++)
+    {
+        tmp[i] = str[end-i];
+    }
+    for (i = 0; i < count; i++)
+    {
+        str[i] = tmp[i];
+    }
+    tmp[i] = '\0';
+    free(tmp);
+    */
 }
 
