30662cb3b3292935384a95427f1ae648bf371a3a
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-02-18 09:39:20 +0300

    lab 2 task 3

---
+++ lab2/src/revert_string/make_libraries.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+gcc -c src/main.c -o bin/main.o
+
+gcc -c src/revert_string.c -o bin/static/revert_string.o
+gcc -c -fPIC src/revert_string.c -o bin/shared/revert_string.o
+
+ar rcs bin/static/librs.a bin/static/revert_string.o
+gcc bin/main.o -Lbin/static -lrs -o bin/statically-linked
+
+gcc -shared bin/shared/revert_string.o -o bin/shared/librs.so
+gcc bin/main.o -Lbin/shared -lrs -o bin/use-shared-library
+
---
+++ lab2/src/revert_string/start_shared.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+LD_LIBRARY_PATH=$(pwd)/bin/shared bin/use-shared-library abc
+
